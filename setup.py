502a35146b028a2d59a9596d410b7d6ed0ae49f7
Pavel Goltsev <p.goltsev@gmail.com>
2017-05-24 11:02:23 +0300

    Added LICENSE.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+setup(
+    name='brew-cask-upgrade',
+    version='0.1',
+    packages=find_packages(),
+    scripts=['brew-cask-upgrade'],
+    author='Pavel Goltsev',
+    author_email='p.goltsev@ya.ru',
+    url="https://github.com/pesh1983/brew_cask_upgrade"
+)
